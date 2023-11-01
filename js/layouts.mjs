2a3b8805ca1af18c7b5e37d0915ac396fc479b03
Kristoffer Grönlund <k@ziran.se>
2023-11-01 13:18:34 +0100

    tweak middlemak-mh

--- js/layouts.mjs
+++ js/layouts.mjs
@@ -252,7 +252,7 @@ export const LAYOUTS = {
     "middlephobia_ansi": "wcdlxzpuoyrsth',neaivgfm;.bkqj",
     "middlemak": "qwldgjfou:asrtpyneihzxcvbkm,./",
     "middlemak-nh": "qwldgjfou:nsrtpyheiazxcvbkm,./",
-    "middlemak-mh": "qwlpzjyou'nsrtgfheiaxvcdbkm-.,",
+    "middlemak-mh": "bwlpzjyou'nsrtgfheiaxvcdqkm-.,",
     "minimak-12": "qwdfkyuil;astrghneopzxcvbjm,./",
     "mmmm": "vmlpqzx,u;strdyfnaiojkwcgbh'./e",
     "moyaimak": "bldwv'foujnrtsmyhaeizxgcqpk/-;",
