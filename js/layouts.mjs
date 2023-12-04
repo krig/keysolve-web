ab733675410fa69192321191d63b4a6fd872ecf5
Kristoffer Grönlund <k@ziran.se>
2023-12-04 12:08:57 +0100

    update krigite

--- js/layouts.mjs
+++ js/layouts.mjs
@@ -252,7 +252,7 @@ export const LAYOUTS = {
     "middlephobia_ansi": "wcdlxzpuoyrsth',neaivgfm;.bkqj",
     "middlemak": "qwldgjfou:asrtpyneihzxcvbkm,./",
     "middlemak-nh": "qwldgjfou:nsrtpyheiazxcvbkm,./",
-    "krigite":   "bldwzjkou,nrtsgyhaeiqxmcvpf'-.",
+    "krigite":   "bldwz'youjnrtsgkhaeiqxmcvpf.-,",
     "nosferateia": "bwlpzjyou'nsrtgfheiaxvcdqkm-,.",
     "minimak-12": "qwdfkyuil;astrghneopzxcvbjm,./",
     "mmmm": "vmlpqzx,u;strdyfnaiojkwcgbh'./e",
